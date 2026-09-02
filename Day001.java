75bbc1f1ca8f954e4827b2fc5675213d6cf663ec
Rei <real70805@gmail.com>
2026-09-02 20:22:56 +0800

    Memahami struktur dasar kode Java & menggunakan semua jenis Komentar
    Implement main class structure with comments for clarity.

---
+++ Day001.java
@@ -0,0 +1,14 @@
+public class main { // ini adalah struktur utama untuk membungkus program yang akan dijalankan, untuk "main" ini di rubah sesuai dengan nama file javanya jvm bisa menemukan dan menjalankan programnya
+
+        public static void main(String[] args) {   //ini adalah untuk menjalankan program utama
+          
+          //int a = 10; selain mengomentari baris bisa juga untuk membuat sintaks tidak dijalaknkan jika ditaro di depan sintaksnya
+            int b = 10; // tercetak karena tanda "//" ada dibelakang barisnya
+
+          /*
+            nah selain garis "//"
+            kita juga bisa menggunakann tanda /* + tab
+            untuk mengomentari beberapa baris sekaligus
+            */
+    } 
+}
