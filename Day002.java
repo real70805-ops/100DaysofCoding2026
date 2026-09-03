84a89ed1cdd74910f5c0ab788ddcb2cc72067fe4
Rei <real70805@gmail.com>
2026-09-03 08:56:49 +0800

    Membuat Biodata Diri dengan Menggunakan println(), print() dan printf()

---
+++ Day002.java
@@ -0,0 +1,7 @@
+public class main {
+  public static void main(String[] args) {
+    System.out.println("nama  : reigar");
+    System.out.printf("Grub  : 11 \n");
+    System.out.print("prodi : Informatika");
+  }
+}
