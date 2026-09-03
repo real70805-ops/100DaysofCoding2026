96b512e81a7ffc35fde80e9eb8780b7727123d6c
Rei <real70805@gmail.com>
2026-09-03 15:20:30 +0800

    Delete Day002.java
    penjelasan 3 jenis print : 
    
    1. print = untuk mencetak keluaran dari variabel ataupun langsung menuliskan dengan tanda "".
    
    2. println = sama aja dengan print tapi bedanya kalau print itu output nya cuman jadi 1 baris, nah untuk println ini tuh output println atau print yang di bawahnya bisa tercetak di bawah dan tidak hanya 1 harus.
    
    3. printf = nah printf ini tuh unik sendiri, ketika di print dan println untuk mencetak variable di antara teks harus menggunakan  tanda +, nah printf ini bisa mencetak variable dengan lebih simple menggunakan %d %f %s %c di dalam tanda kutip. nah untuk pindah baris seperti di println, printf menggunakan \n.

--- Day002.java
+++
@@ -1,7 +0,0 @@
-public class main {
-  public static void main(String[] args) {
-    System.out.println("nama  : reigar");
-    System.out.printf("Grub  : 11 \n");
-    System.out.print("prodi : Informatika");
-  }
-}
