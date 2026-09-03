b854ded043459ee77abfd53f14e31403a3d1d59a
Rei <real70805@gmail.com>
2026-09-03 15:52:01 +0800

    Membuat Biodata Diri dengan Menggunakan println(), print() dan printf()
    Add a Java program to print student information.

---
+++ Day002.java
@@ -0,0 +1,7 @@
+public class main {
+  public static void main(String[] args) {
+    System.out.println("nama  : reigar"); // println ini tuh fungsinya untuk mencetak print dibawahnya dibagian bawah
+    System.out.printf("kelas : A \n"); // printf ini untuk memudahkan dalam memanggil variable di antara teks dengan menggunakan %f %d %s %c, nah untuk mencetak print dibawahnya tercetak dibawah menggunakan \n
+    System.out.print("prodi : Informatika"); // print ini tuh fungsinya cetak teks atau variable tapi hanya cetak hanya bisa tercetak ke samping dan tidak ta enter
+  }
+}
