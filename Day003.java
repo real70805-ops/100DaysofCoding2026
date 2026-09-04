5c550a26484a66f3590819bdf73e9113eaf7e885
Rei <real70805@gmail.com>
2026-09-04 14:34:25 +0800

    Penggunaan Escape Sequence (\n, \t, dan lain lain).

---
+++ Day003.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello\twolrd"); // \t ini untuk menambahkan tab didalam didalam string
+        System.out.print("hello wolrd \n"); // \n ini untuk membuat teks selanjutnya di bawah
+        System.out.println("hello wolrd\b "); // \b ini untuk menghapus 1 karakter di belakang \b nya
+        System.out.println("\"hello wolrd\" "); // \" ini untuk menampilkan karakter "" di dalam teks 
+        System.out.println("\'hello wolrd\'"); // \' ini untuk menampilkan karakter '' di dalam teks
+        System.out.print("hellowolrd ");
+        System.out.println("\rhalo dunia"); // \r ini untuk mengganti karakter yang ada dibelakang \r nya dengan yang didepa \r nya 
+        System.out.println("hello world\f"); // \f ini untuk membuat baris kosong di bawahnya 
+        System.out.println("hello wolrd\\"); // \\ ini untuk mnampilkan \ didalam teks
+
+    }
+}
