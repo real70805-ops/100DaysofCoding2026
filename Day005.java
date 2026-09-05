8d313fea3de434c8f1032a3b380d5d555b2af338
Rei <real70805@gmail.com>
2026-09-06 00:41:38 +0800

    Tipe Data Pecahan (double, float).

---
+++ Day005.java
@@ -0,0 +1,15 @@
+public class java {
+    public static void main(String[] args) {
+    // floaat dan duoble adalah tipe data untuk menyimpan bilangan desimal 
+
+    // 1. float, bisa untuk menampung 6-7 digit bilangan dibelakang koma(harus ada "f" atau "F" di akahir untuk menandao kalo ini tipe data float bukan double)
+    float berat = 70.5f;
+
+    // 2. double, bisa untuk menampung 15-16 digit bilangan dibelakang koma
+    double tinggi =  176.4;
+
+    // output
+    System.out.printf("berat badan saya\t: %f\n", berat);
+    System.out.printf("tinggi badan saya\t: %f\n", tinggi);
+    }
+}
