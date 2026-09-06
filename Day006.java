69c0a6d4ee70defc9fa17243a6f914939b2fb229
Rei <real70805@gmail.com>
2026-09-07 07:22:24 +0800

    Tipe Data Karakter (char) dan Boolean (boolean).

---
+++ Day006.java
@@ -0,0 +1,12 @@
+public class Contoh{
+  public static void main(String[] args) {
+    // Variable 
+    // Tipe data char hanya bisa muat 1 karakter saja
+    char inisial = 'R';
+    // Tipe data boolean hanya bisa menampilkan 2 kondisi yaitu true atau false
+    boolean jagoNgoding = false;
+
+    //output
+    System.out.printf("Saya berinisial %c dan status skill ngoding saya: %b", inisial, jagoNgoding);
+  }
+}
