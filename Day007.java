1bb61082a007cfde76f1fadfe22116b5cb6839a3
Rei <real70805@gmail.com>
2026-09-08 18:12:20 +0800

    Pengenalan Tipe Data String untuk teks.
    Implement a simple Java program to print strings.

---
+++ Day007.java
@@ -0,0 +1,13 @@
+public class java {
+    public static void main(String[] args) throws InterruptedException{
+//  Tipe data String adalah tipe data non primitif yang tidak menyimpan nilai langsung melainkan hanya menyimpan alamat nya atau hanya menampilkan teksnya
+        String  kata1 = "Ga Sombong";
+        String  kata2 = "GA BarBar";
+        String  kata3 = "UNSULBARR!!";
+
+            // output
+         System.out.println(kata1);
+         System.out.println(kata2);
+         System.out.println(kata3);
+    }
+}
