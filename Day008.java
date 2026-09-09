279c9a0aa34c2672b64d244e37256a3187d4540f
Rei <real70805@gmail.com>
2026-09-09 14:57:04 +0800

    Deklarasi, Inisialisasi dan Update Variabel.

---
+++ Day008.java
@@ -0,0 +1,13 @@
+public class java {
+    public static void main(String[] args) throws InterruptedException{
+        //deklarasi variable 
+        String alamat;
+        
+        //inisialisasi variable
+         double tinggi = 170.5;
+
+        // update variable 
+        int umur = 69;
+        umur = 17;
+   }
+}
