43b03ac01c4b64902ffe19e39854cbba6eed006c
Rei <real70805@gmail.com>
2026-09-10 09:47:29 +0800

    Constanta (final variable)

---
+++ Day009.java
@@ -0,0 +1,19 @@
+public class PengenalanString {
+    public static void main(String[] args) {
+      // nilai konstanta adalah nilai yang tidak bisa berubah contohnya seperti ini
+      final int JUMLAH_HARI = 7;
+      final int MAX_NILAI = 100;
+      final int JUMLAH_JAM = 24;
+      final int JUMLAH_MENIT = 60;
+      final int JUMLAH_DETIK = 60;
+      
+      // contoh yang akan error jika ingin dirubah nilainya
+      /*
+      JUMLAH_HARI = 9;
+      MAX_NILAI = 25;
+      JUMLAH_JAM = 67;
+      JUMLAH_MENIT = 29;
+      JUMLAH_DETIK = 196;
+      */
+    }
+}
