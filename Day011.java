16f2caf3b5262fafce77d584eb09d3d1964c8165
Rei <real70805@gmail.com>
2026-09-12 21:28:42 +0800

    Menerima Input dari Keyboard (pengenalan Scanner).
    Implement basic string input functionality using Scanner.

---
+++ Day011.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;  // untuk mengimport dari library java.util untuk memgenali fungsi scanner
+public class PengenalanString {
+    public static void main(String[] args) {
+      String nama;
+
+      // baris dibawah ini berfungsi untuk membuat objek input baru yang bertugas membaca data yang dimasukkan oleh pengguna melalui keyboard.
+      Scanner input = new Scanner(System.in); 
+     // menampilkan teks petunjuk di layar
+      System.out.print("Masukkan nama kamu = ");
+      //mengambil teks yang diketik pengguna lalu menyimpannya ke variabel nama.
+      nama = input.nextLine();
+    }
+}
