d505bbf46c8aa04e434a194d5c6ba68da5ea3379
Rei <real70805@gmail.com>
2026-09-16 18:37:12 +0800

    Operator Aritmatika Perkalian (*) dan Pembagian (/).

---
+++ Day015.java
@@ -0,0 +1,15 @@
+public class day15 {   
+  public static void main(String[] args) {
+    int a = 6;
+    int b = 2;
+    int c = a * b;
+    
+    System.out.println("angka a = "+a);
+    System.out.println("angka b = "+b);
+    System.out.println("Hasil a x b = "+ c);
+    
+    c = a / b;
+    
+    System.out.println("Hasil a : b = "+c);
+      }
+}
