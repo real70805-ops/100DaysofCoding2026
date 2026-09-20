87465fbcf7d81ec2f83ef9fe150fbd34912bb73f
Rei <real70805@gmail.com>
2026-09-20 11:57:46 +0800

    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ Day019.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class slebew {
+    public static void main(String[] args) {
+      Scanner i = new Scanner(System.in);
+
+      System.out.print("Masukkan harga: ");
+      int hAw = i.nextInt();
+      System.out.print("Masukkan diskon: ");
+      double dis = i.nextDouble();
+      
+      System.out.print("\n");
+      
+      double hSD = hAw - (dis / 100 * hAw);
+      int hAk = (int) hSD;
+      
+      System.out.println("Harga awal\t\t: " + hAw);
+      System.out.println("Harga setelah diskon : " + hSD);
+      System.out.println("Harga akhir\t\t: " + hAk);
+    }
+} 
