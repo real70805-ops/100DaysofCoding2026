1335691d9bd7c9ad2f8a001cec037beab7232145
Rei <real70805@gmail.com>
2026-09-26 10:56:46 +0800

    Latihan: Program Menghitung Luas Lingkaran.

---
+++ Day025.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class PasswordBruteForceAnimated {
+    public static void main(String[] args) {
+      Scanner i = new Scanner(System.in);
+      final double PI = 3.14;
+      double r = i.nextDouble();
+
+      double L = PI * r * r;
+
+      System.out.println("Luas : " + L);
+    }
+}
